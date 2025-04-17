7ad46bf9d0263e7acd5baf968a102ca866afa567
mohammed595 <Mohammed9Ne@gmail.com>
2025-04-17 17:16:05 +0300

    chore: Add translated model to (ar, en) massage

---
+++ lib/common/models/translated_text.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'translated_text.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$TranslatedTextImpl _$$TranslatedTextImplFromJson(Map<String, dynamic> json) =>
+    _$TranslatedTextImpl(
+      en: json['en'] as String,
+      ar: json['ar'] as String,
+    );
+
+Map<String, dynamic> _$$TranslatedTextImplToJson(
+        _$TranslatedTextImpl instance) =>
+    <String, dynamic>{
+      'en': instance.en,
+      'ar': instance.ar,
+    };
