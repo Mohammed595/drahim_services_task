052cadfb810cc14380653e6afa7b14e304d816c0
mohammed595 <Mohammed9Ne@gmail.com>
2025-04-18 22:08:38 +0300

    fest: design all screen part

---
+++ lib/features/services/views/screens/services_screen.dart
@@ -0,0 +1,67 @@
+import 'package:drahim_services_task/features/services/view_models/scroll_state_view_model.dart';
+import 'package:drahim_services_task/features/services/view_models/services_view_model/services_provider.dart';
+import 'package:drahim_services_task/features/services/views/widgets/services_app_bar.dart';
+import 'package:drahim_services_task/features/services/views/widgets/services_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ServicesScreen extends ConsumerStatefulWidget {
+  const ServicesScreen({super.key});
+
+  @override
+  ConsumerState<ServicesScreen> createState() => _ServicesScreenState();
+}
+
+class _ServicesScreenState extends ConsumerState<ServicesScreen> {
+  final controller = ScrollController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(_onScroll);
+  }
+
+  void _onScroll() {
+    final isScrolling = controller.position.pixels > 30;
+    ref.read(scrollStateProvider.notifier).setScrolling(isScrolling);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isScrolling = ref.watch(scrollStateProvider);
+    final state = ref.watch(servicesProvider);
+
+    return Scaffold(
+      body: CustomScrollView(
+        controller: controller,
+        slivers: [
+          ServicesAppBar(isScrolling: isScrolling),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 6),
+              child: Text(
+                'Darahim Service',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+            ),
+          ),
+          state.maybeWhen(
+            orElse: () => const SliverToBoxAdapter(child: SizedBox.shrink()),
+            loading: () => const SliverToBoxAdapter(
+                child: Center(child: CircularProgressIndicator())),
+            success: (services) => ServicesList(services: services.services),
+            error: (error) => SliverToBoxAdapter(
+              child: Center(child: Text(error.toString())),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/services/views/widgets/service_item.dart
@@ -0,0 +1,39 @@
+import 'package:drahim_services_task/features/services/data/models/service_model.dart';
+import 'package:drahim_services_task/features/services/views/widgets/sub_services_list.dart';
+import 'package:flutter/material.dart';
+
+class ServiceItem extends StatelessWidget {
+  final Service service;
+
+  const ServiceItem({required this.service, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: Text(
+            service.title.en,
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+        ),
+        DecoratedBox(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Theme.of(context).primaryColor.withOpacity(0.1),
+                blurRadius: 10,
+                offset: const Offset(0, 5),
+              ),
+            ],
+          ),
+          child: SubServicesList(subServices: service.sub_services),
+        ),
+      ],
+    );
+  }
+}
