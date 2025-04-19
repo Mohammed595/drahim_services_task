efcb06dacf50f451403e0cc29009da0110968201
mohammed595 <Mohammed9Ne@gmail.com>
2025-04-20 02:17:24 +0300

    chore: Remove unused Hive imports and initialization from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,11 @@ import 'package:drahim_services_task/core/secrets/env.dart';
 import 'package:drahim_services_task/features/services/views/screens/services_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   DrahimServiceEndpoints.initialize(Env.drahimBaseUrl);
-  final dir = await getApplicationDocumentsDirectory();
-  Hive.init(dir.path);
+
   runApp(
     ProviderScope(
       child: const MyApp(),
